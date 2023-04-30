d8399f14510960aae43d7f852526ff3306067ad7
samber <samber@users.noreply.github.com>
2023-04-30 18:33:52 +0000

    bump v0.2.0

--- version.go
+++ version.go
@@ -1,4 +1,4 @@
 package slogdatadog
 
 const name = "samber/slog-datadog"
-const version = "VERSION" // replaced by .github/workflows/release.yml
+const version = "v0.2.0" // replaced by .github/workflows/release.yml
